84a081d6f30c27da7bf06944d9fef242c7ac63dc
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2023-01-27 17:40:43 -0600

    Verifies normal item sellIn decrements

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,19 +1,21 @@
 package com.gildedrose
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class GildedRoseTest {
-
-    @Test
-    fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
-
+    @ParameterizedTest
+    @CsvSource(
+        "1, 0", // has not expired
+        "0, -1", // expiration day
+        "-1, -2", // expired
+    )
+    fun `updateQuality decrements sellIn by 1`(initialSellIn: Int, expectedSellInt: Int) {
+        val items = arrayOf(Item("normal item", initialSellIn, 7))
+        GildedRose(items).updateQuality()
+        assertThat(items[0].sellIn).isEqualTo(expectedSellInt)
     }
-
 }
 
 
