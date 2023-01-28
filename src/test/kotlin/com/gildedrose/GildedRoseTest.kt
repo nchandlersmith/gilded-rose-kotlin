47bf15854c93b3180a8dba71ce00a952bc3b44c1
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2023-01-28 10:52:14 -0600

    Verify aged brie sellIn decrements

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -38,6 +38,17 @@ internal class GildedRoseTest {
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(expectedQuality)
     }
+    @ParameterizedTest(name = "initialSellIn: {0} expectedQuality: {1}")
+    @CsvSource(
+        "1, 0", // not expired
+        "0, -1", // expiration day
+        "-1, -2", // expired
+    )
+    fun `updateQuality Aged Brie sellIn decrements`(initialSellIn: Int, expectedSellIn: Int) {
+        val items = arrayOf(Item("Aged Brie", initialSellIn, 7))
+        GildedRose(items).updateQuality()
+        assertThat(items.get(0).sellIn).isEqualTo(expectedSellIn)
+    }
 }
 
 
