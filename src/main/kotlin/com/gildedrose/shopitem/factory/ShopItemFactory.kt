04995ad04053a36fd5a368475b9e6405df128f25
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2023-01-28 21:17:23 -0600

    Refactor factory

--- src/main/kotlin/com/gildedrose/shopitem/factory/ShopItemFactory.kt
+++ src/main/kotlin/com/gildedrose/shopitem/factory/ShopItemFactory.kt
@@ -4,16 +4,11 @@ import com.gildedrose.*
 import com.gildedrose.shopitem.*
 
 class ShopItemFactory {
-    fun create(item: Item): ShopItem {
-        if ("Aged Brie" == item.name) {
-            return AgedBrie(item)
+    fun create(item: Item):ShopItem =
+        when (item.name) {
+            "Aged Brie" -> AgedBrie(item)
+            "Backstage passes to a TAFKAL80ETC concert" -> BackStagePasses(item)
+            "Sulfuras, Hand of Ragnaros" -> Sulfurus(item)
+            else -> Normal(item)
         }
-        if ("Backstage passes to a TAFKAL80ETC concert" == item.name) {
-            return BackStagePasses(item)
-        }
-        if ("Sulfuras, Hand of Ragnaros" == item.name) {
-            return Sulfurus(item)
-        }
-        return Normal(item)
-    }
 }
