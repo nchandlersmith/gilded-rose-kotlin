1a4226e286a2f54008d11e35772f5951ca8761f7
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2023-01-29 11:02:48 -0600

    Format tests

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -17,6 +17,7 @@ internal class GildedRoseTest {
         GildedRose(items).updateQuality()
         assertThat(items[0].sellIn).isEqualTo(expectedSellInt)
     }
+
     @ParameterizedTest(name = "sellIn: {0} initialQuality: {1} expectedQuality: {2}")
     @CsvSource(
         "1, 3, 2", // not expired
@@ -28,6 +29,7 @@ internal class GildedRoseTest {
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(expectedQuality)
     }
+
     @ParameterizedTest(name = "sellIn: {0} initialQuality: {1} expectedQuality: {2}")
     @CsvSource(
         "1, 0, 0", // not expired
@@ -41,6 +43,7 @@ internal class GildedRoseTest {
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(expectedQuality)
     }
+
     @ParameterizedTest(name = "initialSellIn: {0} expectedSellIn: {1}")
     @CsvSource(
         "1, 0", // not expired
@@ -52,6 +55,7 @@ internal class GildedRoseTest {
         GildedRose(items).updateQuality()
         assertThat(items.get(0).sellIn).isEqualTo(expectedSellIn)
     }
+
     @ParameterizedTest(name = "sellIn: {0} initialQuality: {1} expectedQuality: {2}")
     @CsvSource(
         "1, 3, 4", // not expired
@@ -63,6 +67,7 @@ internal class GildedRoseTest {
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(expectedQuality)
     }
+
     @ParameterizedTest(name = "sellIn: {0} initialQuality: {1} expectedQuality: {2}")
     @CsvSource(
         "1, 50, 50", // not expired
@@ -71,11 +76,16 @@ internal class GildedRoseTest {
         "-1, 49, 50", // expired
         "-1, 50, 50", // expired
     )
-    fun `updateQuality aged brie quality cannot be greater than 50`(sellIn: Int, initialQuality: Int, expectedQuality: Int) {
+    fun `updateQuality aged brie quality cannot be greater than 50`(
+        sellIn: Int,
+        initialQuality: Int,
+        expectedQuality: Int
+    ) {
         val items = arrayOf(Item("Aged Brie", sellIn, initialQuality))
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(expectedQuality)
     }
+
     @ParameterizedTest(name = "initialSellIn: {0} expectedQuality: {1}")
     @CsvSource(
         "11, 10",
@@ -93,44 +103,57 @@ internal class GildedRoseTest {
         GildedRose(items).updateQuality()
         assertThat(items.get(0).sellIn).isEqualTo(expectedSellIn)
     }
+
     @Test
     fun `updateQuality backstage passes quality increases by 1`() {
         val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 11, 20))
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(21)
     }
+
     @ParameterizedTest(name = "sellIn: {0} initialQuality: {1} expectedQuality {2}")
     @CsvSource(
         "10, 20, 22",
         "9, 20, 22",
         "6, 20, 22"
     )
-    fun `updateQuality backstage passes quality increases by 2`(sellIn: Int, initialQuality: Int, expectedQuality: Int) {
-        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert",sellIn, initialQuality))
+    fun `updateQuality backstage passes quality increases by 2`(
+        sellIn: Int,
+        initialQuality: Int,
+        expectedQuality: Int
+    ) {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", sellIn, initialQuality))
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(expectedQuality)
     }
+
     @ParameterizedTest(name = "sellIn: {0} initialQuality: {1} expectedQuality {2}")
     @CsvSource(
         "5, 20, 23",
         "4, 20, 23",
         "1, 20, 23",
     )
-    fun `updateQuality backstage passes quality increases by 3`(sellIn: Int, initialQuality: Int, expectedQuality: Int) {
-        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert",sellIn, initialQuality))
+    fun `updateQuality backstage passes quality increases by 3`(
+        sellIn: Int,
+        initialQuality: Int,
+        expectedQuality: Int
+    ) {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", sellIn, initialQuality))
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(expectedQuality)
     }
+
     @ParameterizedTest(name = "sellIn: {0} initialQuality: {1} expectedQuality {2}")
     @CsvSource(
         "0, 20, 0",
         "-1, 20, 0",
     )
     fun `updateQuality backstage passes quality drops to 0`(sellIn: Int, initialQuality: Int, expectedQuality: Int) {
-        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert",sellIn, initialQuality))
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", sellIn, initialQuality))
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(expectedQuality)
     }
+
     @ParameterizedTest(name = "sellIn: {0} initialQuality: {1} expectedQuality {2}")
     @CsvSource(
         "11, 50, 50",
@@ -140,23 +163,30 @@ internal class GildedRoseTest {
         "5, 49, 50",
         "5, 48, 50",
     )
-    fun `updateQuality backstage passes quality cannot be greater than 50`(sellIn: Int, initialQuality: Int, expectedQuality: Int) {
-        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert",sellIn, initialQuality))
+    fun `updateQuality backstage passes quality cannot be greater than 50`(
+        sellIn: Int,
+        initialQuality: Int,
+        expectedQuality: Int
+    ) {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", sellIn, initialQuality))
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(expectedQuality)
     }
+
     @Test
     fun `updateQuality sulfurus sellIn does not decrement`() {
         val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", 10, 80))
         GildedRose(items).updateQuality()
         assertThat(items.get(0).sellIn).isEqualTo(10)
     }
+
     @Test
     fun `updateQuality sulfurus quality does not decrement`() {
         val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", 10, 70))
         GildedRose(items).updateQuality()
         assertThat(items.get(0).quality).isEqualTo(70)
     }
+
     @Test
     fun `updateQuality preserves item name`() {
         val name = "some name"
