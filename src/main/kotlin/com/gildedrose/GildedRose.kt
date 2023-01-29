4be703b8c494d21a25335b60f7a8c733a09574b5
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2023-01-28 20:14:18 -0600

    Add classes of item types

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,75 +2,72 @@ package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
-
         for (i in items.indices) {
-            if (isDegradingItem(i)) {
-                safeDecrementQuality(i)
-            } else {
-                safeIncrementQuality(i)
-                if (isBackstagePasses(i) && items[i].sellIn < 11) {
-                    safeIncrementQuality(i)
-                }
-                if (isBackstagePasses(i) && items[i].sellIn < 6) {
-                    safeIncrementQuality(i)
-                }
+            if (isSulfurus(i)) {
+                Sulfurus(items[i]).apply()
+                return
             }
-
-            safeDecrementSellIn(i)
-
-            if (isExpired(i)) {
-                if (isAgedBrie(i)) {
-                    safeIncrementQuality(i)
-                } else if (isBackstagePasses(i)) {
-                        setQualityTo0(i)
-                } else {
-                        safeDecrementQuality(i)
-                }
+            if (isAgedBrie(i)) {
+                AgedBrie(items[i]).apply()
+                return
             }
+            if (isBackstagePasses(i)) {
+                BackStagePasses(items[i]).apply()
+                return
+            }
+            Normal(items[i]).apply()
         }
-    }
-
-    private fun isDegradingItem(i: Int) = !isAgedBrie(i) && !isBackstagePasses(i) && !isSulfurus(i)
-
-    private fun safeDecrementSellIn(i: Int) {
-        if (!isSulfurus(i)) {
-            decrementSellIn(i)
-        }
-    }
-
-    private fun isExpired(i: Int) = items[i].sellIn < 0
-
-    private fun decrementSellIn(i: Int) {
-        items[i].sellIn -= 1
-    }
-
-    private fun safeIncrementQuality(i: Int) {
-        if (items[i].quality < 50) {
-            incrementQuality(i)
-        }
-    }
-
-    private fun safeDecrementQuality(i: Int) {
-        if (items[i].quality > 0) {
-            decrementQuality(i)
-        }
-    }
-
-    private fun incrementQuality(i: Int) {
-        items[i].quality += 1
-    }
-
-    private fun setQualityTo0(i: Int) {
-        items[i].quality = 0
-    }
-
-    private fun decrementQuality(i: Int) {
-        items[i].quality -= 1
     }
 
     private fun isAgedBrie(i: Int) = "Aged Brie" == items[i].name
     private fun isBackstagePasses(i: Int) = "Backstage passes to a TAFKAL80ETC concert" == items[i].name
     private fun isSulfurus(i: Int) = "Sulfuras, Hand of Ragnaros" == items[i].name
 
+}
+
+class Sulfurus(var item: Item) {
+    fun apply() {
+    }
+}
+
+class AgedBrie(var item: Item) {
+    fun apply() {
+        item.quality += 1
+        if (item.sellIn < 1) {
+            item.quality += 1
+        }
+        item.sellIn -=1
+        item.quality = minOf(50, item.quality)
+    }
+}
+
+class BackStagePasses(var item: Item) {
+    fun apply() {
+        if (item.sellIn <= 0) {
+            item.sellIn -= 1
+            item.quality = 0
+            return
+        }
+            item.quality += 1
+            if (item.sellIn < 11) {
+                item.quality += 1
+            }
+            if (item.sellIn < 6) {
+                item.quality += 1
+            }
+            item.sellIn -= 1
+            item.quality = minOf(50, item.quality)
+    }
+}
+
+class Normal(var item: Item) {
+    fun apply() {
+        item.quality -= 1
+        if (item.sellIn < 1) {
+            item.quality -= 1
+        }
+        item.sellIn -=1
+        item.quality = maxOf(0, item.quality)
+    }
 }
 
